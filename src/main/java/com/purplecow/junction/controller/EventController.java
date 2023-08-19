edbbf8051dc12c621d604706de9ab36ccba79eb0
hyoung <ycat01@naver.com>
2023-08-19 23:23:37 +0900

    [FIX] develop cicd 제거

--- src/main/java/com/purplecow/junction/controller/EventController.java
+++ src/main/java/com/purplecow/junction/controller/EventController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "users", description = "유저")
+@Tag(name = "events", description = "유저")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
